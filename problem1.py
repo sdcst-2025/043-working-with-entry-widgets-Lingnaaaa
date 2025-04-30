0dc77ba4c1fc104504bba0bd4d3aed8de39decfa
Lingnaaaa <lingnah794@deltalearns.ca>
2025-04-30 12:08:31 -0700

    I finished

--- problem1.py
+++ problem1.py
@@ -13,12 +13,8 @@ Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
 import tkinter as tk
-import math
 w=tk.Tk()
-eoutput=tk.StringVar()
-eoutput.set("   is the answer")
-qoutput=tk.StringVar()
-qoutput.set("   is the other answer")
+
 
 def run(e):
     print(e)
@@ -32,9 +28,12 @@ def run(e):
             x2=(0-n1-((n1**2)-(4*n2))**0.5)/2
             x=round(x,2)
             x2=round(x2,2)
-            print(f"The x are {x} and {x2}")
-            e3.insert(0,x)
-            e4.insert(0,x2)
+            e3.delete(0,tk.END) 
+            e4.delete(0,tk.END)
+            s1=f"x - {x}" if x>=0 else f"x + {abs(x)}"
+            s2=f"x - {x2}" if x2>=0 else f"x + {abs(x2)}"
+            e3.insert(0,s1)
+            e4.insert(0,s2)
     except:
         print("error,try anther number")
 
@@ -47,15 +46,15 @@ l2=tk.Label(w,text="Number 2")
 e2=tk.Entry(w,width=15)
 b1=tk.Button(w,text="Click for solve")
 b1.bind('<Button>',run)
-e3=tk.Entry(w,width=30,textvariable=eoutput)
-e4=tk.Entry(w,width=30,textvariable=qoutput)
+e3=tk.Entry(w,width=30)
+e4=tk.Entry(w,width=30)
 
-l0.grid(row=0,column=0)
+l0.grid(row=0,column=0,columnspan=2)
 l1.grid(row=1,column=0)
-e1.grid(row=2,column=0)
-l2.grid(row=1,column=1)
+e1.grid(row=1,column=1)
+l2.grid(row=2,column=0)
 e2.grid(row=2,column=1)
-b1.grid(row=3,column=0)
-e3.grid(row=4,column=0,)
-e4.grid(row=5,column=0)
+b1.grid(row=3,column=0,columnspan=2)
+e3.grid(row=4,column=1)
+e4.grid(row=5,column=1)
 w.mainloop()
